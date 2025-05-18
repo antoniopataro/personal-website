877645d265a9e546ebb98bfc7ef2238e358b705e
antoniopataro <antoniopatarodev@gmail.com>
2025-05-18 16:29:39 -0300

    feat: first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended",
+      "plugin:import/recommended",
+      "plugin:import/typescript",
+    ],
+    ignorePatterns: ["./dist/**", "./node_modules/**"],
+    parser: "@typescript-eslint/parser",
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  }),
+];
+
+export default eslintConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+    </main>
+  );
+}
